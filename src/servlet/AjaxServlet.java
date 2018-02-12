9122047c1c962eb96626497105d1be43bfff45c1
liKun <13923813589@163.com>
2018-02-12 10:35:59 +0800

    加jquery

--- src/servlet/AjaxServlet.java
+++ src/servlet/AjaxServlet.java
@@ -1,15 +1,15 @@
 package servlet;
 
-import javax.servlet.ServletConfig;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.sql.*;
 
 /**
  * Created by Administrator on 2018/1/28 0028.
@@ -26,27 +26,75 @@ public class AjaxServlet extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
         //获取web.xml中的初始值
-        System.out.println("-----config---------"+getServletConfig());
-        ServletConfig servletConfig=getServletConfig();
-        System.out.println("-----test-----"+servletConfig.getInitParameter("test"));
-        System.out.println("-----test1-------"+servletConfig.getInitParameter("test1"));
-        //普通get/post请求（post请求传送json字符串的时候，不能用此方法）
-        System.out.println("-----name-------"+req.getParameter("username"));
-        System.out.println("-----password-------"+req.getParameter("password"));
-
-        //前端提交的数据是json格式的字符串数据时，需要以下方式接收数据
-        ServletInputStream is = req.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(is,"utf-8"));
-        String line=null;
-        while((line=br.readLine())!=null){
-            System.out.println("------------------------------------------values-----------------------------");
-            System.out.println(line); //{"username":"汤姆","password":"123"}
+//        ServletConfig servletConfig=getServletConfig();
+//        //普通get/post请求（post请求传送json字符串的时候，不能用此方法）
+//        System.out.println("-----name-------"+req.getParameter("username"));
+//        System.out.println("-----password-------"+req.getParameter("password"));
+//
+//        //前端提交的数据是json格式的字符串数据时，需要以下方式接收数据
+//        ServletInputStream is = req.getInputStream();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(is,"utf-8"));
+//        String line=null;
+//        while((line=br.readLine())!=null){
+//            System.out.println("------------------------------------------values-----------------------------");
+//            System.out.println(line); //{"username":"汤姆","password":"123"}
+//        }
+//
+//        PrintWriter pw=resp.getWriter();
+//        pw.write("SUCCESS!!!!!");
+//        pw.close();
+////        req.getRequestDispatcher("WEB-INF/views/success.html").forward(req,resp);
+        BASE64Decoder decoder = new BASE64Decoder();
+        BASE64Encoder encoder = new BASE64Encoder();
+        String bb64 = "";
+        String addr = "jdbc:oracle:thin:@192.168.169.200:1521:orcl";
+        String user = "fsgadtfj";
+        String psw = "fsgadtfj2017";
+        String sql = "select t.test,t.img_file from TEST_9_30 t where t.test='a351c6f12a12484fb500caae1a215d01'";
+        Connection conn = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection(addr, user, psw);
+            Statement stat = conn.createStatement();
+            ResultSet res = stat.executeQuery(sql);
+            while (res.next()) {
+                if (!bb64.equals("")) {
+                    bb64 = bb64 + "<SP>";
+                }
+                bb64 = bb64+ res.getString("test");
+                Blob blob = res.getBlob("img_file");
+                if (blob != null) {
+                    InputStream in = blob.getBinaryStream();
+                    long nLen = blob.length();
+                    int nSize = (int) nLen;
+                    byte[] data = new byte[nSize];
+                    in.read(data);
+                    in.close();
+//                    bb64 = bb64 + ",img_file:" +new String(data,"utf8");
+                    bb64 = bb64 + ";;data:image/png;base64," + encoder.encode(data);
+                }
+            }
+            if (bb64.equals("")) {
+                bb64 = "0,查无信息!";
+            }
+        } catch (SQLException e) {
+            bb64 = "-5," + e.getMessage();
+            e.printStackTrace();
+        } catch (Exception e) {
+            bb64 = "-5,err!";
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                bb64 = "-5,err!";
+                e.printStackTrace();
+            }
         }
-
-        PrintWriter pw=resp.getWriter();
-        pw.write("SUCCESS!!!!!");
-        pw.close();
-//        req.getRequestDispatcher("WEB-INF/views/success.html").forward(req,resp);
+        req.setAttribute("dataString", bb64);
+        req.getRequestDispatcher("WEB-INF/views/result.jsp").forward(req, resp);
     }
 
     @Override
---
+++ web/WEB-INF/views/result.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/2/12 0012
+  Time: 上午 9:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>SHOW RESULT</title>
+</head>
+<body>
+<h3>${dataString.split(';;')[0]}</h3><br>
+<img src="${dataString.split(';;')[1]}">
+
+<script type="text/javascript" src="libs/jquery-2.1.1.js"></script>
+<script>
+
+</script>
+</body>
+</html>
